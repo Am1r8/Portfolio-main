c01b83c059ece5c277479ec29dd1573b493bfa3c
AlPHA__830 <68626539+CoachAlpha@users.noreply.github.com>
2022-05-24 14:20:12 -0400

    added emojies

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -11,11 +11,11 @@ const sumfetch = async (args: string[]): Promise<string> => {
 爵 Repo: <u><a href="${config.repo}" target="_blank">Github repo</a></u>
 -----------
  CONTACT 
- Email: <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
- Github: <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
- Linkedin: <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-Twitter: <u><a href="https://twitter.com/${config.social.twitter}" target="_blank">twitter.com/${config.social.twitter}</a></u>
-Instagram: <u><a href="https://www.instagram.com/${config.social.instagram}" target="_blank">https://www.instagram.com/${config.social.instagram}</a></u>
+  Email: <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+  Github: <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+📸 Instagram: <u><a href="https://www.instagram.com/${config.social.instagram}" target="_blank">https://www.instagram.com/${config.social.instagram}</a></u>
+  Linkedin: <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+🐦 Twitter: <u><a href="https://twitter.com/${config.social.twitter}" target="_blank">twitter.com/${config.social.twitter}</a></u>
 
   `;
 };
