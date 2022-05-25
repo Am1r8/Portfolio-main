ca8da66c5f00a5257a0a8701d943dcbc05e17be1
AlPHA__830 <68626539+CoachAlpha@users.noreply.github.com>
2022-05-24 22:11:47 -0400

    ready for host\

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    plugins: ['prettier'],
+    extends: ['next/core-web-vitals'],
+    rules: {
+        'no-console': 'error',
+        'prettier/prettier': 'warn',
+        'react-hooks/exhaustive-deps': 'off',
+    },
+};
