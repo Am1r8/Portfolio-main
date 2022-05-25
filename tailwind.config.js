1dfda6c9b7c6b036fa8a2710017886bd2d641663
AlPHA__830 <68626539+CoachAlpha@users.noreply.github.com>
2022-05-24 21:09:35 -0400

    published

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
+/* eslint-disable prettier/prettier */
 const { colors } = require('./config.json');
 
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
-  darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      ...colors,
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx}',
+        './src/components/**/*.{js,ts,jsx,tsx}',
+    ],
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            ...colors,
+        },
+        extend: {},
     },
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
