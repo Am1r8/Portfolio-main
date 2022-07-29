3427dc853beca1b9d07bec53532516cd82f9fe2a
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2022-07-29 16:18:21 -0700

    major design changes

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -53,7 +53,7 @@ export const resume = async (args: string[]): Promise<string> => {
 
 export const email = async (args: string[]): Promise<string> => {
   window.open(`mailto:${config.email}`);
-  return `Opening mailto:${config.email} ...`;
+  return `Opening mailto: ${config.email} ...`;
 };
 
 export const github = async (args: string[]): Promise<string> => {
