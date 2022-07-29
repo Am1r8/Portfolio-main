0fe2b4e05767cc81799f1916680f34aab25fe6cd
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2022-07-29 16:22:14 -0700

    major design changes

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -13,7 +13,7 @@ export const projects = async (args: string[]): Promise<string> => {
       (repo) =>
         `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
     )
-    .join('\n');
+    .join('\n') + "\n\nAND MORE BY WRITING 'repo'";
 };
 
 export const quote = async (args: string[]): Promise<string> => {
